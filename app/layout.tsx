4622c206fef38284f587622cbc4f3b05b8993a9f
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-05-26 11:06:29 +0100

    Use dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
                 <Providers
                     themeProps={{
                         attribute: "class",
-                        defaultTheme: "system",
+                        defaultTheme: "dark",
                         enableSystem: true,
                     }}
                 >
