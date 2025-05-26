8b43a4ab722139381a0d824174d9cb87d908fe81
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-05-26 16:12:02 +0100

    Properly setup QueryClientProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,6 +7,7 @@ import * as React from "react";
 
 import { HeroUIProvider } from "@heroui/system";
 import { ToastProvider } from "@heroui/toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export interface ProvidersProps {
     children: React.ReactNode;
@@ -23,11 +24,16 @@ declare module "@react-types/shared" {
 
 export const Providers = ({ children, themeProps }: ProvidersProps) => {
     const router = useRouter();
-
+    const client = React.useMemo(() => {
+        return new QueryClient();
+    }, []);
     return (
-        <HeroUIProvider navigate={router.push}>
-            <ToastProvider />
-            <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-        </HeroUIProvider>
+        <QueryClientProvider client={client}>
+            <HeroUIProvider navigate={router.push}>
+                <ToastProvider />
+                <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+            </HeroUIProvider>
+        </QueryClientProvider>
+
     );
 };
